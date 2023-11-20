0b09137bb591a11a767c9e1226f74be40eb589e8
Predrag Radojicic <mail2pedja@gmail.com>
2023-11-20 12:56:04 +0100

    Added a test for testing main menu. Location: cypress/e2e/menu.

---
+++ cypress/support/page_objects/menu-elements.js
@@ -0,0 +1,52 @@
+
+class TestMenuElements {
+    static openPage(jsonArray) {
+
+      // Iterate over each object in the JSON array.
+      jsonArray.forEach((element) => {
+
+        // Construct the selector.
+        const element_on_the_menu = `${element.menu_element}`;
+        const page_selection = `${element.page_is_open}`;
+  
+        // Find the HTML element by class and click on it.
+        cy.get(element_on_the_menu).click();
+        // Confirm that the element is active on the menu.
+        cy.get(element_on_the_menu).should('have.class', 'active');
+        // Confirm that the page is open.
+        cy.get(page_selection).should('exist');
+  
+      });
+    }
+  }
+  
+  export default TestMenuElements;
+
+
+
+
+
+
+// // Create a class to handle element interactions
+// class ElementInteraction {
+//     // Function to interact with HTML elements based on JSON data
+//     static interactWithElements() {
+//       // Read the JSON file from the fixtures folder
+//       cy.fixture('menu-elements.json').then((jsonArray) => {
+//         // Iterate over each object in the JSON array
+//         jsonArray.forEach((element) => {
+//           // Construct the selector using class
+//           const selector = `${element.Page}`;
+  
+//           // Find the HTML element by class and click on it
+//           cy.get(selector).click();
+  
+//           // Add additional assertions or interactions as needed
+//         });
+//       });
+//     }
+//   }
+  
+//   // Export the class
+//   export default ElementInteraction;
+  
